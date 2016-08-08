1a290b3cff54e1145d24c0522e9d5133d837e3bc
Oscar <oscar.centeno@outlook.com>
2016-08-07 23:39:36 -0600

    Completo el paso 7 al mostrar un ejemplo con la inversion de las dependencias.

---
+++ ConsoleApplication1/7 Con inversion de dependencias/InformacionParaUnMenorDeEdadMedico.cs
@@ -0,0 +1,33 @@
+﻿using ConPolimorfismo;
+using System;
+
+namespace ConsoleApplication1
+{
+    internal class InformacionParaUnMenorDeEdadMedico : InformacionFormateada
+    {
+        public override string OU
+        {
+            get
+            {
+                return "OU=MENOR DE EDAD";
+            }
+        }
+
+        public override string Proposito
+        {
+            get
+            {
+                Console.WriteLine("Indique el proposito, por favor:");
+                return Console.ReadLine();
+            }
+        }
+
+        public override string SerialNumber
+        {
+            get
+            {
+                return $"SERIALNUMBER=MENOR-{Identificacion}";
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConPolimorfismo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InformacionFormateada laInformacion = new InformacionParaUnMenorDeEdadMedico();
+            laInformacion.Nombre = "Juan";
+            laInformacion.PrimerApellido = "Sanchez";
+            laInformacion.SegundoApellido = "Sosa";
+            laInformacion.Identificacion = "4-4323-2345";
+
+            string elSujeto = new SujetoFormateado(laInformacion).ComoTexto();
+
+            Console.WriteLine("El sujeto es: ");
+            Console.Write(elSujeto);
+            Console.ReadLine();
+        }
+    }
+}
